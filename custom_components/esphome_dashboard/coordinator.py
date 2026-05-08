9ddf1d2b39ae3165aff79305c8209298993b4393
Paul Strawder <paul@strawder.info>
2026-05-08 07:19:24 +0200

    Log device keys

--- custom_components/esphome_dashboard/coordinator.py
+++ custom_components/esphome_dashboard/coordinator.py
@@ -50,6 +50,7 @@ class ESPHomeDashboardCoordinator(DataUpdateCoordinator[dict[str, ConfiguredDevi
 
             _LOGGER.error("RECEIVED %d devices", len(configured_devices))
             _LOGGER.error("DEBUG: device names=%s", list(device["name"] for device in configured_devices))
+            _LOGGER.error("DEBUG: device keys=%s", list(configured_devices[0].keys()) if configured_devices else "None")
             _LOGGER.error("DEBUG: full device data 0=%s", configured_devices[0] if configured_devices else "None")
             _LOGGER.error("DEBUG: raw devices_data=%s", devices_data)
             # Return devices indexed by their name
