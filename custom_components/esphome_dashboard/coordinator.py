e045a0fb9141ef264997aab91fc73195c417346f
Paul Strawder <paul@strawder.info>
2026-05-08 07:34:16 +0200

    Final cleanup and v1.4.2 release

--- custom_components/esphome_dashboard/coordinator.py
+++ custom_components/esphome_dashboard/coordinator.py
@@ -50,13 +50,6 @@ class ESPHomeDashboardCoordinator(DataUpdateCoordinator[dict[str, ConfiguredDevi
 
             _LOGGER.error("RECEIVED %d devices", len(configured_devices))
             _LOGGER.error("DEBUG: device names=%s", list(device["name"] for device in configured_devices))
-            import json; _LOGGER.error("DEBUG: FULL_JSON_START" + json.dumps(devices_data) + "FULL_JSON_END")
-            _LOGGER.error("DEBUG: raw devices_data=%s", devices_data)
-            # Return devices indexed by their name
-            return {device["name"]: device for device in configured_devices}
-        except aiohttp.ClientResponseError as err:
-            _LOGGER.error("HTTP ERROR: %s", err)
-            if err.status in (401, 403):
                 raise ConfigEntryAuthFailed(
                     "Authentication failed. Please update your credentials."
                 ) from err
